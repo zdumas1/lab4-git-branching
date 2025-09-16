be87a901ca5ea9bf84617ca021480fd80102cc21
zdumas1 <zdumas1@emich.edu>
2025-09-16 15:27:06 +0000

    Hero path. Adding for if you go left

--- story.py
+++ story.py
@@ -10,7 +10,10 @@ def intro():
 
 def left_path():
     print("You walk left and find a mysterious glowing sword stuck in a stone.")
-
+    print("You grab the sword. You hear a growling in front of you. Somewhere within the forest.")
+    print("A dog runs out of the forest, followed by a GIANT BEAST!!")
+    print("You challenge the beast, having courage with your new weapon. The beast is slain")
+    print("You walk away with both a new friend and a new weapon")
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
 
